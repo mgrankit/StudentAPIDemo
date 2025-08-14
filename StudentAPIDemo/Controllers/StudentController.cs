c69d44365e0895814bc7eb181115a82f1c122d37
ankitmagar <mgrankit68@gmail.com>
2025-08-14 21:23:10 +0545

    Add project files.

---
+++ StudentAPIDemo/Controllers/StudentController.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace StudentAPIDemo.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentController : ControllerBase
+    {
+       static List<Student> students = new List<Student>();
+        // GET: api/<StudentController>
+        [HttpGet]
+        public IEnumerable<Student> Get()
+        {
+            return students;
+        }
+
+        // GET api/<StudentController>/5
+        [HttpGet("{id}")]
+        public Student Get(int id)
+        {
+            return students.FirstOrDefault(s => s.StudentId == id);
+        }
+
+        // POST api/<StudentController>
+        [HttpPost]
+        public void Post([FromBody] Student value)
+        {
+            students.Add(value);
+        }
+
+        // PUT api/<StudentController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] Student value)
+        {
+            int num = students.FindIndex(s => s.StudentId == id);
+            if(num>=0)
+            {
+                students[num] = value;
+            }
+
+        }
+
+        // DELETE api/<StudentController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            students.RemoveAll(s => s.StudentId == id);
+        }
+    }
+}
---
+++ StudentAPIDemo/Student.cs
@@ -0,0 +1,8 @@
+﻿namespace StudentAPIDemo
+{
+    public class Student
+    {
+       public int StudentId {  get; set; }
+        public string StudentName { get; set; }
+    }
+}
